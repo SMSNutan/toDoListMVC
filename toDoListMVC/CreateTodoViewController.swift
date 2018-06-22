c2022f582defcf07dec219219dd9980ee435c712
Nutan Niraula <nutan.smartmobe@gmail.com>
2018-06-22 11:21:03 +0545

    [feat] adds datepicker input accessory view

--- toDoListMVC/CreateTodoViewController.swift
+++ toDoListMVC/CreateTodoViewController.swift
@@ -11,17 +11,23 @@ import UIKit
 class CreateTodoViewController: UIViewController {
     @IBOutlet weak var taskTitleTextField: UITextField!
     @IBOutlet weak var taskDescriptionTextField: UITextField!
-    @IBOutlet weak var taskExpiryDate: UITextField!
+    @IBOutlet weak var taskExpiryDateTextField: UITextField!
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var networkCallStatusLabel: UILabel!
     
     var activityIndicator: UIActivityIndicatorView!
     var alert: UIAlertController!
+    var dateToolBar: UIToolbar!
+    var datePicker = UIDatePicker()
     
     @IBAction func onSaveButtonTapped(_ sender: Any) {
         //Network call to save model
-        
+        guard taskExpiryDateTextField.text != "" && taskDescriptionTextField.text != "" && taskExpiryDateTextField.text != ""
+            else {
+                networkCallStatusLabel.text = "Please fill all the required fields"
+                return
+        }
         // initializing url for post request
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         
@@ -48,14 +54,14 @@ class CreateTodoViewController: UIViewController {
                 print(sentPost)
                 //changing code execution to main thread for displaying data in ui
                 DispatchQueue.main.async {
-                    self?.networkCallStatusLabel.text = "Call Successful"
+                    self?.networkCallStatusLabel.text = "Task Created"
                     self?.networkCallStatusLabel.textColor = .blue
                     self?.hideActivityIndicator()
                 }
             } catch {
                 //handling error
                 DispatchQueue.main.async {
-                    self?.networkCallStatusLabel.text = "Call Failed"
+                    self?.networkCallStatusLabel.text = "Failed to save task"
                     self?.networkCallStatusLabel.textColor = .red
                     self?.hideActivityIndicator()
                 }
@@ -73,6 +79,7 @@ class CreateTodoViewController: UIViewController {
     private func addVisualStyleToView() {
         addCornerRadiusToContainerView()
         addCornerRadiusToSaveButton()
+        addDatePickerInInputAccessoryView()
     }
     
     private func addCornerRadiusToContainerView() {
@@ -85,8 +92,8 @@ class CreateTodoViewController: UIViewController {
         saveButton.clipsToBounds = true
     }
     
-    //View Animation Code
-    func showActivityIndicator() {
+    //Activity Indicator Code
+    private func showActivityIndicator() {
         alert = UIAlertController(title: nil, message: "Saving...", preferredStyle: .alert)
         let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
         loadingIndicator.hidesWhenStopped = true
@@ -96,9 +103,36 @@ class CreateTodoViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    func hideActivityIndicator() {
+    private func hideActivityIndicator() {
         alert.dismiss(animated: true, completion: nil)
     }
     
+    //add toolbar
+    private func addDatePickerInInputAccessoryView() {
+        dateToolBar = UIToolbar()
+        dateToolBar.sizeToFit()
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: nil, action: #selector(onDoneTapped))
+        dateToolBar.setItems([doneBtn], animated: true)
+        datePicker.backgroundColor = .white
+        datePicker.datePickerMode = .date
+        datePicker.minimumDate = Date()
+        taskExpiryDateTextField.inputAccessoryView = dateToolBar
+        taskExpiryDateTextField.inputView = datePicker
+    }
+    
+    @objc private func onDoneTapped() {
+        taskExpiryDateTextField.text = createDateString(fromDate: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    private func createDateString(fromDate date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        let dateString = dateFormatter.string(from: date)
+        let date = dateFormatter.date(from:dateString)!
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        return dateFormatter.string(from:date)
+    }
 }
 
